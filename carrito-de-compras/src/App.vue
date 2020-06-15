897e124094876c5f17a3dfc3a45ca46fbcf8d77c
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-15 20:58:27 -0300

    Creating the component Product.

--- carrito-de-compras/src/App.vue
+++ carrito-de-compras/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="row">
+      <div class="col-md-7">
+        <div class="r">
+          <div class="col-md-6">
+            <producto></producto>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Producto from './components/Producto'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Producto
   }
 }
 </script>
