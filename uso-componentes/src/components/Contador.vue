0645e873dadf6eccf2c02af0f62d8230c7290206
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-15 19:13:27 -0300

    Adding uso-componentes

---
+++ uso-componentes/src/components/Contador.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button @click='contar'>{{ contador }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Contador',
+    data(){
+        return  {
+            contador: 0
+        }
+    },
+    methods: {
+        contar: function() {
+            this.contador++;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ uso-componentes/src/components/Plan.vue
@@ -0,0 +1,33 @@
+<template>
+    <div @click="seleccionar" class="plan" :class="{'plan-activo':seleccionado}">
+        <div class="descripcion">
+            <span class="titulo">{{ nombre }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Plan',
+    props: ['nombre'],
+    data() {
+        return {
+            seleccionado: false
+        }
+    },
+    methods: {
+        seleccionar() {
+            if(!this.seleccionado) {
+                this.$emit('select', this.nombre);
+                this.seleccionado = true;
+            } else {
+                this.seleccionado = false;
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
