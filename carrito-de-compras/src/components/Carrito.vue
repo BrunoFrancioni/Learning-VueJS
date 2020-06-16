6d48577071843ae9b18eda883edcd05c9056f47d
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-16 14:20:44 -0300

    Component Carrito.

---
+++ carrito-de-compras/src/components/Carrito.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="">
+        <ul class="list-group">
+            <li class="list-group-item" v-for="item in items" :key="item.id">
+                {{ item.titulo }} - $ {{ item.precio }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Carrito',
+    props: ['items']
+}
+</script>
+
+<style>
+
+</style>
