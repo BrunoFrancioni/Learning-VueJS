507fcc1b1de7b826edf7f69ef357d34184ac04e8
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-18 09:43:33 -0300

    Refactoring nav bar.

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link> |
-      <router-link to="/users">Usuario</router-link>
-    </div>
+  <div id="app" class="container">
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <router-link to="/" class="nav-link active">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/about" class="nav-link">About</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/contact" class="nav-link">Contact</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/users" class="nav-link">Usuario</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/login" class="nav-link">Login</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="signup" class="nav-link">Signup</router-link>
+      </li>
+    </ul>
+
     <transition>
       <keep-alive>
         <router-view/>
@@ -15,6 +30,8 @@
 </template>
 
 <style>
+@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
