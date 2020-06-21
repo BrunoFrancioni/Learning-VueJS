777779fdbe5c4dc4e24c30ff824b802dcf148729
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-21 13:06:40 -0300

    vuex example finished.

--- vuex-example/src/store/index.js
+++ vuex-example/src/store/index.js
@@ -5,11 +5,35 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    msg: 'Vuex desde cero.',
+    nombre: 'Juan',
+    apellido: 'Pérez',
+    amigos: []
   },
   mutations: {
+    addAmigo(state, amigo){
+      state.amigos = [amigo,...state.amigos];
+    },
+    delAmigo(state, index) {
+      state.amigos.splice(index, 1);
+    }
   },
   actions: {
+    addAmigoAction(context, amigo) {
+      context.commit('addAmigo', amigo);
+    },
+    delAmigoAction(context, index) {
+      context.commit('delAmigo', index);
+    }
   },
   modules: {
+  },
+  getters: {
+    mensaje(state){
+      return state.msg;
+    },
+    nombreCompleto(state) {
+      return `${state.nombre} ${state.apellido}`;
+    }
   }
 })
