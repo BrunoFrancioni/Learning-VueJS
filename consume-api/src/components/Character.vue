662e643bdbedf936d3eea5f73d8f3ee9668141bc
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-24 13:09:16 -0300

    Component to represents the characters finiched.

---
+++ consume-api/src/components/Character.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="column is-12-mobile is-4-desktop is-4-tablet">
+        <div class="card">
+            <div class="card-header">
+                <img v-bind:src="character.image" v-bind:alt="character.name">
+            </div>
+            
+            <div class="card-content">
+                <h3 class="title is-size-4">{{ character.name }}</h3>
+                <button class="button is-success is-rounded is-small">Ver más</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Character',
+    props: ["character"]
+}
+</script>
+
+<style>
+
+</style>
