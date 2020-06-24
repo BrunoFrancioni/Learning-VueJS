e8bb2ba54942300423a27d0cafdd8dd1dc61e562
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-24 11:48:59 -0300

    Fetching firsts data.

--- consume-api/src/App.vue
+++ consume-api/src/App.vue
@@ -1,26 +1,39 @@
 <template>
   <div id="app">
-    
+    <h1 class="title">Consultar Datos</h1>
+    <button v-on:click="fetch">Consultar</button>
+
+    <div v-for="character in characters" :key="character.id">
+      {{ character }}
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    
+  data: () => {
+    return {
+      characters: []
+    }
+  },
+  methods: {
+    async fetch() {
+      try {
+        const result = await axios.get("https://rickandmortyapi.com/api/character")
+
+        this.characters = result.data.results;
+        console.log(result.data);
+      } catch(error) {
+        console.log(error);
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
