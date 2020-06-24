0350b5fe5d11c1801c4b18bf71e7f310a7ed0a92
Bruno Francioni <brunofrancioni1998@gmail.com>
2020-06-24 13:09:46 -0300

    Showing the characters works.

--- consume-api/src/App.vue
+++ consume-api/src/App.vue
@@ -1,23 +1,40 @@
 <template>
   <div id="app">
-    <h1 class="title">Consultar Datos</h1>
-    <button v-on:click="fetch">Consultar</button>
+    <div class="hero is-white is-gradient is-bold">
+      <div class="hero-body">
+        <h1 class="title">
+          <span class="has-text-success">R&M</span>
+          <span class="subtitle">Personajes</span>
+        </h1>
 
-    <div v-for="character in characters" :key="character.id">
-      {{ character }}
+        <button class="button is-success is-rounded" v-on:click="fetch">Consultar</button>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="columns is-desktop is-mobile is-tablet is-multiline is-centered">
+        <character v-for="character in characters" :key="character.id" v-bind:character="character"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Character from './components/Character'
 
 export default {
   name: 'App',
+  components: {
+    Character
+  },
   data: () => {
     return {
       characters: []
     }
+  },
+  created() {
+    this.fetch();
   },
   methods: {
     async fetch() {
